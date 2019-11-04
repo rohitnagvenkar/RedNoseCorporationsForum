9b1dce73574d29e0128fa52248a0d13e83c5a7fa
Rohit Nagvenkar <rohitsnagvenkar@gmail.com>
2019-11-03 19:57:32 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedNoseCorporationsForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
